6a6df0c1865b9b60c97b4c7f7b8c69215eac6231
David Delgado Lopez <ddelg112@fiu.edu>
2022-09-15 14:52:23 -0400

    Flixer part 2

---
+++ Flixer/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixer
+//
+//  Created by David Delgado on 9/14/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
